fad98bfc9a0afa582ee0c28429b6a0d076848c2b
Chad Scherrer <chad.scherrer@gmail.com>
2021-04-27 14:59:53 -0700

    init

--- src/SampleChainsAbstractMCMC.jl
+++ src/SampleChainsAbstractMCMC.jl
@@ -1,5 +1,87 @@
 module SampleChainsAbstractMCMC
 
-# Write your package code here.
+using SampleChains
+using AbstractMCMC
+
+
+
+
+using Reexport
+@reexport using SampleChains
+using NestedTuples
+using ElasticArrays
+using ConcreteStructs
+using MappedArrays
+using Random
+using TupleVectors
+
+using TupleVectors: chainvec
+
+export AbstractMCMCChain
+
+@concrete struct  AbstractMCMCChain{T} <: AbstractChain{T}
+    samples     # :: AbstractVector{T}
+    info        # Per-sample metadata, type depends on sampler used
+    meta        # Metadata associated with the sample as a whole
+    state       
+    transform
+end
+
+function AbstractMCMCChain(samples::AbstractVector{T}, info, meta, state, transform) where {T}
+    return AbstractMCMCChain{T}(samples, info, meta, state, transform)
+end
+
+TupleVectors.summarize(ch::AbstractMCMCChain) = summarize(samples(ch))
+
+function gettransform(chain::AbstractMCMCChain)
+    return getfield(chain, :transform)
+end
+
+function SampleChains.pushsample!(chain::AbstractMCMCChain, val, state)
+    tr = getfield(chain, :transform)
+    samplemap = tr.samplemap
+    statemap = tr.statemap
+
+    push!(samples(chain), samplemap(val))
+    push!(info(chain), statemap(state))
+end
+
+function SampleChains.step!(chain::AbstractMCMCChain)
+    st = getfield(chain, :state)
+    mt = getfield(chain, :meta)
+    rng = st.rng
+    state = st.state[]
+    model = mt.model
+    sampler = mt.sampler
+    val, state = AbstractMCMC.step(rng, model, sampler, state)
+end
+
+function SampleChains.initialize!(rng::Random.AbstractRNG, ::Type{AbstractMCMCChain}, model::AbstractMCMC.AbstractModel, spl::AbstractMCMC.AbstractSampler, samplemap, statemap)
+    (val, state) = AbstractMCMC.step(rng, model, spl)
+
+    sample0 = samplemap(val)
+    info0 = statemap(state)
+
+    samples = chainvec(sample0)
+    info = chainvec(info0)
+    meta = (model = model, sampler = spl)
+    state = (rng=rng, state=Ref(state))
+    transform = (; samplemap, statemap)
+    
+    AbstractMCMCChain(samples, info, meta, state, transform)
+end
+
+function SampleChains.initialize!(::Type{AbstractMCMCChain}, model::AbstractMCMC.AbstractModel, spl::AbstractMCMC.AbstractSampler, samplemap, statemap)
+    rng = Random.GLOBAL_RNG
+    return initialize!(rng, AbstractMCMCChain, model, spl, samplemap, statemap)
+end
+
+function SampleChains.drawsamples!(chain::AbstractMCMCChain, n::Int=1000)
+    @cleanbreak for j in 1:n
+        val, state = step!(chain)
+        pushsample!(chain, val, state)
+    end 
+    return chain
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,29 @@ using SampleChainsAbstractMCMC
 using Test
 
 @testset "SampleChainsAbstractMCMC.jl" begin
-    # Write your tests here.
+    using AdvancedMH
+    using Distributions
+    using MCMCChains
+    using AbstractMCMC
+
+    # Generate a set of data from the posterior we want to estimate.
+    data = rand(Normal(0, 1), 30)
+
+    # Define the components of a basic model.
+    insupport(θ) = θ[2] >= 0
+    dist(θ) = Normal(θ[1], θ[2])
+    density(θ) = insupport(θ) ? sum(logpdf.(dist(θ), data)) : -Inf
+
+    # Construct a DensityModel.
+    model = DensityModel(density)
+
+    # Set up our sampler with a joint multivariate Normal proposal.
+    spl = RWMH(MvNormal(2,1))
+
+    using SampleChainsAbstractMCMC
+
+    chain = initialize!(AbstractMCMCChain, model, spl, x -> (x = x.params[1], y=x.params[2]), x -> x.params)
+    drawsamples!(chain, 9999)
+
+
 end
